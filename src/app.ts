afc4137569012a4d07a4e93df39a8e1d189bc481
aluno <e023aluno@outlook.com>
2026-03-19 22:02:40 -0300

    adicionando pastas no viwes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express  from "express";
 import produtoRoutes from './routes/produtoRoutes';
 import { pool } from './config/database';
+import path from "path";
 
 const app = express()
+app.use(express.static(path.join(
+    __dirname,'views' )));
+
+
 
 app.use(express.json());
 app.use('/api', produtoRoutes);
